6d6a42de9db22bc73672972dc970d05dda2e3e99
David de Kloet <dskloet@gmail.com>
2023-11-18 20:27:53 +0100

    Apply changes to all players if there is more than one player.

--- content.js
+++ content.js
@@ -27,40 +27,55 @@ function onKeyDown(e) {
   e.preventDefault();
 }
 
+function withVideoPlayer(callback) {
+  const videoPlayers = Array.from(document.querySelectorAll('video'));
+  console.log('Found', videoPlayers.length, 'video players.');
+  videoPlayers.forEach(callback);
+}
+
 function isEditable(element) {
   return element.isContentEditable ||
       element.tagName == 'INPUT' ||
       element.tagName == 'TEXTAREA';
 }
 
-async function togglePause() {
-  const videoPlayer = document.querySelector('video');
-  const isPaused = videoPlayer.paused;
-  console.log('isPaused', isPaused);
-  await sleep(250);
-  console.log('videoPlayer.paused', videoPlayer.paused);
-  if (videoPlayer.paused !== isPaused) {
-    console.log('The website itself already toggled pause.');
-    return;
-  }
-  if (videoPlayer.paused) {
-    console.log('unpausing');
-    videoPlayer.play();
-  } else {
-    console.log('pausing');
-    videoPlayer.pause();
-  }
+function togglePause() {
+  withVideoPlayer(async (videoPlayer) => {
+
+    const isPaused = videoPlayer.paused;
+    console.log('isPaused', isPaused);
+    await sleep(250);
+    console.log('videoPlayer.paused', videoPlayer.paused);
+    if (videoPlayer.paused !== isPaused) {
+      console.log('The website itself already toggled pause.');
+      return;
+    }
+    if (videoPlayer.paused) {
+      console.log('unpausing');
+      videoPlayer.play();
+    } else {
+      console.log('pausing');
+      videoPlayer.pause();
+    }
+
+  });
 }
 
 function adjustSpeed(delta) {
-  var videoPlayer = document.querySelector('video');
-  videoPlayer.playbackRate += delta;
-  console.log(`playbackRate = ${videoPlayer.playbackRate}`);
+  withVideoPlayer((videoPlayer) => {
+
+    videoPlayer.playbackRate += delta;
+    console.log(`playbackRate = ${videoPlayer.playbackRate}`);
+
+  });
 }
 
 function seek(delta) {
-  var videoPlayer = document.querySelector('video');
-  videoPlayer.currentTime += delta;
+  withVideoPlayer((videoPlayer) => {
+
+    videoPlayer.currentTime += delta;
+
+  });
 }
 
 function sleep(millis) {
@@ -70,13 +85,16 @@ function sleep(millis) {
 }
 
 function skip() {
-  var videoPlayer = document.querySelector('video');
-  const delta = 5;
-  videoPlayer.playbackRate += delta;
-  console.log(`playbackRate = ${videoPlayer.playbackRate}`);
-  sleep(1000).then(() => {
-    videoPlayer.playbackRate -= delta;
+  withVideoPlayer((videoPlayer) => {
+
+    const delta = 5;
+    videoPlayer.playbackRate += delta;
     console.log(`playbackRate = ${videoPlayer.playbackRate}`);
+    sleep(1000).then(() => {
+      videoPlayer.playbackRate -= delta;
+      console.log(`playbackRate = ${videoPlayer.playbackRate}`);
+    });
+
   });
 
   (document.querySelector('.ytp-ad-skip-button') ||
